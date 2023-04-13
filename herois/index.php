7c77c2ef86d3cc39f625c948030a62c166f54db3
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-04-12 22:14:51 -0300

    Update index.php

--- herois/index.php
+++ herois/index.php
@@ -13,16 +13,38 @@
 <body>
 <form method="post">
 		<label for="hero">Sleciome o Heroi:</label><br><br>
-		<select name="hero" id="hero">
+		<select name="hero">
 			<option value="superman">Superman</option>
 			<option value="batman">Batman</option>
 			<option value="homem-ferro">Homem de Ferro</option>
 			<option value="captain-america">Captain America</option>
 		</select><br><br>
 		<button type="submit">Enviar</button>
-	</form>
+	</form><br><br>
 
-	
+	<?php
+    $hero = $_POST['hero'] ?? "";
+    if($hero == "superman"){
+        echo "<img width='150' src='superman.png'>";
+    }else if($hero == "superman"){
+        echo "<img width='150' src='superman.png'>";
+    }
+	if($hero == "batman"){
+        echo "<img width='150' src='batman.png'>";
+    }else if($hero == "batman"){
+        echo "<img width='150' src='batman.png'>";
+    }
+	if($hero == "homem-ferro"){
+        echo "<img width='150' src='ironman.png'>";
+    }else if($hero == "homem-ferro"){
+        echo "<img width='150' src='ironman.png'>";
+    }
+	if($hero == "captain-america"){
+        echo "<img width='150' src='america.png'>";
+    }else if($hero == "captain-america"){
+        echo "<img width='150' src='america.png''>";
+    }
+    ?>
 </body>
 
 </html>
