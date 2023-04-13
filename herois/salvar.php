4c1cc85a47b8d21628ba855567092c243bd88b78
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-04-12 21:13:48 -0300

    Update salvar.php

--- herois/salvar.php
+++ herois/salvar.php
@@ -1,6 +1,20 @@
 <?php
-$capitaoamerica =$_POST['img/america.png'];
-echo $_POST['img/america.png'];
+// Verifica se o formulário foi enviado
+if (isset($_POST['hero'])) {
+    // Obtém o herói selecionado pelo usuário
+    $hero = $_POST['hero'];
+
+    // Cria a classe CSS correspondente ao herói selecionado
+    $class = str_replace('-', ' ', $hero);
+    $class = ucwords($class);
+    echo '<div class="hero-image" id="" . $hero . ""></div>';
+    echo '<h2>' . $class . '</h2>';
+}
+
+
+
+
+
 
 
 
